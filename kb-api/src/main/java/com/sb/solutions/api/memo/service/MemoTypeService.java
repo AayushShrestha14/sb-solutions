1fde9b523194cef125ae8e09c869d4324d907a07
sushil <np.msushil@gmail.com>
2019-03-23 17:04:41 +0545

    Add MemoType Service

---
+++ kb-api/src/main/java/com/sb/solutions/api/memo/service/MemoTypeService.java
@@ -0,0 +1,14 @@
+package com.sb.solutions.api.memo.service;
+
+import java.util.List;
+
+import com.sb.solutions.api.memo.entity.MemoType;
+import com.sb.solutions.core.enums.Status;
+import com.sb.solutions.core.service.BaseService;
+
+public interface MemoTypeService extends BaseService<MemoType> {
+
+    List<MemoType> findByStatus(Status status);
+
+    List<MemoType> saveAll(List<MemoType> types);
+}
---
+++ kb-api/src/main/java/com/sb/solutions/api/memo/service/MemoTypeServiceImpl.java
@@ -0,0 +1,52 @@
+package com.sb.solutions.api.memo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.sb.solutions.api.memo.entity.MemoType;
+import com.sb.solutions.api.memo.repository.MemoTypeRepository;
+import com.sb.solutions.core.enums.Status;
+
+@Service
+public class MemoTypeServiceImpl implements MemoTypeService {
+
+    private final MemoTypeRepository repository;
+
+    public MemoTypeServiceImpl(@Autowired MemoTypeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<MemoType> findByStatus(Status status) {
+        return repository.findByStatus(status);
+    }
+
+    @Override
+    public List<MemoType> saveAll(List<MemoType> types) {
+        return repository.saveAll(types);
+    }
+
+    @Override
+    public List<MemoType> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public MemoType findOne(Long id) {
+        return repository.getOne(id);
+    }
+
+    @Override
+    public MemoType save(MemoType memoType) {
+        return repository.save(memoType);
+    }
+
+    @Override
+    public Page<MemoType> findAllPageable(MemoType memoType, Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+}
