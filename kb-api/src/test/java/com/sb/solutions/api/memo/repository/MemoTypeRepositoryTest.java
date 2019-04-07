8d4821ffbaa6df5e32c68a30f75915d208de4fa1
sushil <np.msushil@gmail.com>
2019-04-07 22:27:45 +0545

    Remove usage TestEntityManager in Jpa Unit Test as it is not working while running test through mvn command
    Use dbunit instead to setup base data for testing of JPA repository

--- kb-api/src/test/java/com/sb/solutions/api/memo/repository/MemoTypeRepositoryTest.java
+++ kb-api/src/test/java/com/sb/solutions/api/memo/repository/MemoTypeRepositoryTest.java
@@ -7,19 +7,16 @@ import static org.hamcrest.Matchers.notNullValue;
 
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.sb.solutions.BaseJpaTest;
 import com.sb.solutions.api.memo.entity.MemoType;
 import com.sb.solutions.core.enums.Status;
 
+@DatabaseSetup("/dataset/memo/memo-config.xml")
 public class MemoTypeRepositoryTest extends BaseJpaTest {
-
-    @Autowired
-    private TestEntityManager entityManager;
 
     @Autowired
     private MemoTypeRepository repository;
@@ -27,25 +24,8 @@ public class MemoTypeRepositoryTest extends BaseJpaTest {
     public MemoTypeRepositoryTest() {
     }
 
-    @Before
-    public void loadMemoTypes() {
-        final MemoType demo = new MemoType();
-        demo.setName("Demo Type");
-
-        final MemoType demo2 = new MemoType();
-        demo2.setName("Demo Type 2");
-
-        final MemoType demo3 = new MemoType();
-        demo3.setName("Demo Type 3");
-        demo3.setStatus(Status.INACTIVE);
-
-        entityManager.persistAndFlush(demo);
-        entityManager.persistAndFlush(demo2);
-        entityManager.persistAndFlush(demo3);
-    }
-
     @Test
-    public void testFindAllShouldReturnTwoRecords(){
+    public void testFindAllShouldReturnTwoRecords() {
         List<MemoType> memoTypes = repository.findAll();
 
         assertThat(memoTypes, hasSize(3));
@@ -60,7 +40,7 @@ public class MemoTypeRepositoryTest extends BaseJpaTest {
     }
 
     @Test
-    public void testFindByStatusGivenActiveShouldReturnActiveMemoTypes () {
+    public void testFindByStatusGivenActiveShouldReturnActiveMemoTypes() {
         List<MemoType> activeTypes = repository.findByStatus(Status.ACTIVE);
 
         assertThat(activeTypes, hasSize(2));
