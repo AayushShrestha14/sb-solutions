2f20aa9df2e019654c1f78d3a780855df712f343
Bibek Shrestha <rawseek@gmail.com>
2019-04-22 00:26:43 +0545

    API to edit schemes and company

--- kb-web/src/main/java/com/sb/solutions/web/eligibility/scheme/SchemeController.java
+++ kb-web/src/main/java/com/sb/solutions/web/eligibility/scheme/SchemeController.java
@@ -8,11 +8,14 @@ import com.sb.solutions.core.utils.CustomPageable;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,6 +28,8 @@ import javax.validation.Valid;
 @AllArgsConstructor
 public class SchemeController {
 
+    private final Logger logger = LoggerFactory.getLogger(SchemeController.class);
+
     private final SchemeService schemeService;
 
     private final GlobalExceptionHandler globalExceptionHandler;
@@ -33,6 +38,7 @@ public class SchemeController {
     final public ResponseEntity<?> addScheme(@Valid @RequestBody Scheme scheme
             , @PathVariable Long companyId, BindingResult bindingResult) {
         globalExceptionHandler.constraintValidation(bindingResult);
+        logger.debug("Request to save new scheme.");
         final Scheme savedScheme = schemeService.save(scheme);
         if (savedScheme == null) return new RestResponseDto().failureModel("Oops! Something went wrong");
         return new RestResponseDto().successModel(scheme);
@@ -46,8 +52,20 @@ public class SchemeController {
     @GetMapping
     final public ResponseEntity<?> getSchemes(@RequestParam("page") int page, @RequestParam("size") int size
     , @PathVariable Long companyId) {
+        logger.debug("Request to get list of schemes.");
         return new RestResponseDto().successModel(schemeService.
                 findAllPageable(companyId, new CustomPageable().pageable(page, size)));
+    }
+
+    @PutMapping(path = "/{schemeId}")
+    final public ResponseEntity<?> updateSchemes(@PathVariable long companyId, @PathVariable long schemeId
+            , @RequestBody Scheme scheme
+            , BindingResult bindingResult) {
+        globalExceptionHandler.constraintValidation(bindingResult);
+        logger.debug("Request to update the scheme with id [{}]", schemeId);
+        final Scheme updatedScheme = schemeService.save(scheme);
+        if (updatedScheme == null) return new RestResponseDto().failureModel("Oops! Something went wrong.");
+        return new RestResponseDto().successModel(updatedScheme);
     }
 
 }
