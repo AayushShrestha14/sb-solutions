98cc2302603d87a6ee958d49ad7a20b7d2237831
Rujan Maharjan <rujnmahrzn@gmail.com>
2019-04-23 15:29:48 +0545

    refresh bean in run time

--- kb-core/src/main/java/com/sb/solutions/core/config/security/ResourceServerConfig.java
+++ kb-core/src/main/java/com/sb/solutions/core/config/security/ResourceServerConfig.java
@@ -2,8 +2,6 @@ package com.sb.solutions.core.config.security;
 
 import com.sb.solutions.core.config.security.roleAndPermission.RoleAndPermissionDao;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -30,7 +28,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     }
 
     @Override
-    @RefreshScope
     public void configure(HttpSecurity http) throws Exception {
         restrictUrl(http);
         http
