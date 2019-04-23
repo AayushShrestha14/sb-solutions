dbcd963de9680e62e860afe89639cb1b89d8a7ee
davidranamagar <davidranamagar@gmail.com>
2019-04-23 18:55:05 +0545

    loan_config and sub segment mapped

--- kb-api/src/main/java/com/sb/solutions/api/segments/subSegment/entity/SubSegment.java
+++ kb-api/src/main/java/com/sb/solutions/api/segments/subSegment/entity/SubSegment.java
@@ -1,5 +1,6 @@
 package com.sb.solutions.api.segments.subSegment.entity;
 
+import com.sb.solutions.api.loanConfig.entity.LoanConfig;
 import com.sb.solutions.api.segments.segment.entity.Segment;
 import com.sb.solutions.core.enitity.BaseEntity;
 import com.sb.solutions.core.enums.Status;
@@ -8,6 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Data
@@ -22,4 +24,11 @@ public class SubSegment extends BaseEntity<Long> {
     private Segment segment;
 
     private Status status;
+
+    @ManyToMany
+    @JoinTable(name = "sub_segment_loan_config",
+            joinColumns = {@JoinColumn(name = "sub_segment_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "loan_config_id", referencedColumnName = "id")})
+    private Set<LoanConfig> loanConfig;
+
 }
