1e1424d22e0486305f8cb7d5fcd810418b06a97a
Rujan Maharjan <rujnmahrzn@gmail.com>
2019-04-30 20:22:04 +0545

    db name cp_master

--- kb-web/src/main/java/com/sb/solutions/web/CpSolutionApplication.java
+++ kb-web/src/main/java/com/sb/solutions/web/CpSolutionApplication.java
@@ -6,11 +6,8 @@ import com.sb.solutions.api.address.municipalityVdc.service.MunicipalityVdcServi
 import com.sb.solutions.api.address.province.service.ProvinceService;
 import com.sb.solutions.api.basehttp.BaseHttp;
 import com.sb.solutions.api.basehttp.BaseHttpRepo;
-import com.sb.solutions.api.rolePermissionRight.entity.Role;
-import com.sb.solutions.api.user.entity.User;
 import com.sb.solutions.api.user.repository.UserRepository;
 import com.sb.solutions.core.config.security.SpringSecurityAuditorAware;
-import com.sb.solutions.core.enums.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -29,7 +26,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
-import java.util.Date;
 
 /**
  * @author Rujan Maharjan on 12/27/2018
@@ -70,26 +66,12 @@ public class CpSolutionApplication extends SpringBootServletInitializer {
     }
 
 
-
     @PostConstruct
     public void initialize() {
         if (userRepository.findAll().isEmpty()) {
             ClassPathResource schemaResource = new ClassPathResource("patch.sql");
             ResourceDatabasePopulator populator = new ResourceDatabasePopulator(schemaResource);
             populator.execute(dataSource);
-//            Role role = new Role();
-//            role.setId(1L);
-//            User user = new User();
-//            user.setName("SPADMIN");
-//            user.setUserName("SPADMIN");
-//            user.setStatus(Status.ACTIVE);
-//            user.setLastModifiedAt(new Date());
-//            user.setEmail("admin@admin.com");
-//            user.setRole(role);
-//            user.setPassword(passwordEncoder.encode("admin1234"));
-//            userRepository.save(user);
-
-
             schemaResource = new ClassPathResource("oauth.sql");
             populator = new ResourceDatabasePopulator(schemaResource);
             populator.execute(dataSource);
