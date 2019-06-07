366252e33ec97c767fb3ef23e8d157025111bac0
Rujan Maharjan <rujnmahrzn@gmail.com>
2019-06-07 13:04:49 +0545

    updated mapper of customer loan

--- kb-web/src/main/java/com/sb/solutions/web/loan/v1/mapper/Mapper.java
+++ kb-web/src/main/java/com/sb/solutions/web/loan/v1/mapper/Mapper.java
@@ -1,5 +1,10 @@
 package com.sb.solutions.web.loan.v1.mapper;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.gson.Gson;
 import com.sb.solutions.api.Loan.LoanStage;
 import com.sb.solutions.api.Loan.entity.CustomerLoan;
 import com.sb.solutions.api.Loan.service.CustomerLoanService;
@@ -10,7 +15,9 @@ import com.sb.solutions.web.loan.v1.dto.LoanActionDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Rujan Maharjan on 6/4/2019
@@ -22,6 +29,7 @@ public class Mapper {
 
     private final CustomerLoanService customerLoanService;
     private final UserService userService;
+    private final Gson gson;
 
 
     public CustomerLoan ActionMapper(LoanActionDto loanActionDto) {
@@ -30,19 +38,42 @@ public class Mapper {
         User currentUser = userService.getAuthenticated();
         User receivedBy = userService.findByRoleAndBranch(loanActionDto.getToUser(), currentUser.getBranch());
         customerLoan.setLoanType(LoanType.NEW_LOAN);
-        LoanStage loanStage = new LoanStage();
-        List<LoanStage> previousList = customerLoan.getPreviousStageList();
-        if ( customerLoan.getCurrentStage().getId() != null) {
-            previousList.add(loanStage);
+        List previousList = customerLoan.getPreviousList();
+        List previousListTemp = new ArrayList();
+
+        if (customerLoan.getCurrentStage().getId() != null) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+            Map<String, String> tempLoanStage = objectMapper.convertValue(customerLoan.getCurrentStage(), Map.class);
+            try {
+
+                if (previousList != null) {
+                    for (int i = 0; i < previousList.size(); i++) {
+                        try {
+                            Map<String, String> previous = objectMapper.convertValue(previousList.get(i), Map.class);
+
+                            previousListTemp.add(objectMapper.writeValueAsString(previous));
+                        } catch (JsonProcessingException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                String jsonValue = objectMapper.writeValueAsString(tempLoanStage);
+                previousListTemp.add(jsonValue);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
         }
 
-
+        customerLoan.setPreviousStageList(previousListTemp.toString());
+        LoanStage loanStage = customerLoan.getCurrentStage();
         loanStage.setDocAction(loanActionDto.getDocAction());
 
         loanStage.setFromUser(currentUser);
         loanStage.setToUser(receivedBy);
         customerLoan.setCurrentStage(loanStage);
-        customerLoan.setPreviousStageList(previousList);
+        customerLoan.setPreviousList(previousListTemp);
         return customerLoan;
 
     }
