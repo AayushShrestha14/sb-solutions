839fa08d3f524f573a73ad4c8c25bdb9d3bb9156
sushil <np.msushil@gmail.com>
2019-06-11 01:40:06 +0545

    Add HttpMessageNotReadableException handler

--- kb-core/src/main/java/com/sb/solutions/core/exception/handler/RequestExceptionHandler.java
+++ kb-core/src/main/java/com/sb/solutions/core/exception/handler/RequestExceptionHandler.java
@@ -2,6 +2,7 @@ package com.sb.solutions.core.exception.handler;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -9,12 +10,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
 @ControllerAdvice
 public class RequestExceptionHandler {
@@ -46,6 +51,31 @@ public class RequestExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> messageNotReadableExceptionHandler(
+        HttpMessageNotReadableException ex) {
+
+        logger.error("Can not parse request", ex);
+
+        final List<Violation> violations = processMessageError(ex);
+
+        final Map<String, Object> response = Maps.newHashMap();
+
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
+
+        if (null != violations) {
+            response.put("message", "Can not convert to valid object");
+
+            response.put("errorCount", violations.size());
+            response.put("errors", violations);
+        } else {
+            response.put("message", ex.getLocalizedMessage());
+        }
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
     private List<Violation> processError(MethodArgumentNotValidException ex) {
         final BindingResult rs = ex.getBindingResult();
 
@@ -57,12 +87,33 @@ public class RequestExceptionHandler {
 
             for (FieldError error : propertyResult.getFieldErrors()) {
                 final Violation violation = new Violation(error.getField(),
+                    error.getRejectedValue(),
                     error.getDefaultMessage());
 
                 violations.add(violation);
             }
 
             return violations;
+        }
+
+        return null;
+    }
+
+    private List<Violation> processMessageError(HttpMessageNotReadableException ex) {
+
+        if (ex.getMostSpecificCause() instanceof InvalidFormatException) {
+
+            final InvalidFormatException formatException = (InvalidFormatException) ex
+                .getMostSpecificCause();
+
+            final String field = formatException.getPath().stream().map(Reference::getFieldName)
+                .collect(
+                    Collectors.joining(","));
+
+            final Violation violation = new Violation(field, formatException.getValue(),
+                formatException.getOriginalMessage());
+
+            return Lists.newArrayList(violation);
         }
 
         return null;
--- kb-core/src/main/java/com/sb/solutions/core/exception/handler/Violation.java
+++ kb-core/src/main/java/com/sb/solutions/core/exception/handler/Violation.java
@@ -4,14 +4,20 @@ public class Violation {
 
     private final String field;
     private final String message;
+    private final Object rejectedValue;
 
-    public Violation(String field, String message) {
+    public Violation(String field, Object rejectedValue, String message) {
         this.field = field;
+        this.rejectedValue = rejectedValue;
         this.message = message;
     }
 
     public String getField() {
         return field;
+    }
+
+    public Object getRejectedValue() {
+        return rejectedValue;
     }
 
     public String getMessage() {
