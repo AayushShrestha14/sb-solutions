6785308f71ec2b9823d9bf24b0bd338f48cc8b66
Rujan Maharjan <rujnmahrzn@gmail.com>
2019-07-09 10:41:42 +0545

    delete customer loan

--- kb-api/src/main/java/com/sb/solutions/api/loan/service/CustomerLoanServiceImpl.java
+++ kb-api/src/main/java/com/sb/solutions/api/loan/service/CustomerLoanServiceImpl.java
@@ -2,9 +2,12 @@ package com.sb.solutions.api.loan.service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +22,7 @@ import com.sb.solutions.api.user.entity.User;
 import com.sb.solutions.api.user.service.UserService;
 import com.sb.solutions.core.enums.DocAction;
 import com.sb.solutions.core.enums.DocStatus;
+import com.sb.solutions.core.enums.RoleType;
 import com.sb.solutions.core.exception.ServiceValidationException;
 
 /**
@@ -30,6 +34,7 @@ public class CustomerLoanServiceImpl implements CustomerLoanService {
 
     private final CustomerLoanRepository customerLoanRepository;
     private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(CustomerLoanService.class);
 
 
     public CustomerLoanServiceImpl(@Autowired CustomerLoanRepository customerLoanRepository,
@@ -136,6 +141,31 @@ public class CustomerLoanServiceImpl implements CustomerLoanService {
         final CustomerLoanSpecBuilder customerLoanSpecBuilder = new CustomerLoanSpecBuilder(s);
         final Specification<CustomerLoan> specification = customerLoanSpecBuilder.build();
         return customerLoanRepository.findAll(specification, pageable);
+    }
+
+    @Override
+    public CustomerLoan delCustomerLoan(Long id) {
+        Optional<CustomerLoan> customerLoan = customerLoanRepository.findById(id);
+        if (!customerLoan.isPresent()) {
+            logger.info("No Loan Found of Id {}", id);
+            throw new ServiceValidationException("No Loan Found of Id " + id);
+        }
+        if (!customerLoan.get().getCurrentStage().getDocAction().equals(DocAction.DRAFT)) {
+            logger.info("Loan can not be deleted it is in stage", id);
+            throw new ServiceValidationException("Loan can not be deleted it is in stage");
+        }
+        User u = userService.getAuthenticated();
+        if (u.getRole().getRoleType().equals(RoleType.MAKER)) {
+            customerLoanRepository
+                .deleteByIdAndCurrentStageDocAction(id, DocAction.DRAFT);
+            if (!customerLoanRepository.findById(id).isPresent()) {
+                return new CustomerLoan();
+            } else {
+                throw new ServiceValidationException("Oops Something went wrong");
+            }
+        } else {
+            throw new ServiceValidationException("You don't have access to delete file");
+        }
     }
 }
 
