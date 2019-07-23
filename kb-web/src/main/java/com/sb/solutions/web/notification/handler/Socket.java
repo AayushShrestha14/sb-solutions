7293bb71b29ebd3bfbc55ddde932980a72366bfc
yunish_stha <yunishshrestha8@gmail.com>
2019-07-23 16:50:50 +0545

    added end user notification backend configuration and controller

--- kb-web/src/main/java/com/sb/solutions/web/notification/handler/Socket.java
+++
@@ -1,5 +0,0 @@
-package com.sb.solutions.web.notification.handler;
-
-public class Socket  {
-
-}
--- kb-web/src/main/java/com/sb/solutions/web/notification/handler/SocketController.java
+++ kb-web/src/main/java/com/sb/solutions/web/notification/handler/SocketController.java
@@ -1,15 +1,12 @@
 package com.sb.solutions.web.notification.handler;
 
 import com.sb.solutions.api.userNotification.entity.Sender;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@CrossOrigin("*")
+@RestController
 public class SocketController {
     private final SimpMessagingTemplate simpMessagingTemplate;
 
