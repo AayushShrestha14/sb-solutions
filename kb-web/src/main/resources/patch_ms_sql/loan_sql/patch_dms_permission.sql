47a55f937714fffb504b7ac81a4f896479a807ac
Rujan Maharjan <rujnmahrzn@gmail.com>
2019-08-04 14:58:11 +0545

    refactor patch according to 2014 mssql server

--- kb-web/src/main/resources/patch_ms_sql/loan_sql/patch_dms_permission.sql
+++ kb-web/src/main/resources/patch_ms_sql/loan_sql/patch_dms_permission.sql
@@ -1,10 +1,11 @@
+
 BEGIN
-DECLARE @count smallint
-SET @count = (Select count(*) from permission where id = 2)
+DECLARE @countPermission smallint
+SET @countPermission = (Select count(*) from permission where id = 2)
 DELETE FROM permission_api_list where permission_id in (4,8,11)
 DELETE FROM role_permission_rights WHERE permission_id in (4,8,11)
 DELETE FROM permission WHERE id in (4,8,11)
-if(@count = 0)
+if(@countPermission = 0)
 BEGIN
 SET IDENTITY_INSERT permission ON
 INSERT  INTO permission (id,  permission_name, fa_icon, front_url, orders, status) VALUES
@@ -25,32 +26,52 @@ SET IDENTITY_INSERT role_permission_rights ON
 
 
 BEGIN
-DECLARE @count smallint
+DECLARE @countLoan smallint
 DECLARE @countPending smallint
-SET @count = (Select count(*) from url_api where id = 40)
+DECLARE @checkPermPending smallint
+DECLARE @checkPermLoan smallint
+SET @countloan = (Select count(*) from url_api where id = 40)
+SET @checkPermLoan = (Select count(*) from permission_api_list where api_list_id = 40)
+SET @checkPermPending = (Select count(*) from permission_api_list where api_list_id = 46)
+
 SET @countPending = (Select count(*) from url_api where id = 46)
-if(@count = 0)
+if(@countloan = 0)
 BEGIN
 SET IDENTITY_INSERT url_api ON
  INSERT  INTO url_api (id, api_url, type)
 values (40, '/v1/config/getAll', 'LOAN CATEGORY')
-INSERT  INTO permission_api_list(permission_id, api_list_id)
-values (17, 40)
 SET IDENTITY_INSERT url_api off
 END
 
-if(@countPending = 0)
+if(@checkPermLoan = 0)
+BEGIN
+
+INSERT  INTO permission_api_list(permission_id, api_list_id)
+values (17, 40)
+
+END
+
+if(@checkPermPending = 0)
 BEGIN
 SET IDENTITY_INSERT url_api ON
  INSERT  INTO url_api (id, api_url, type)
 values (46,'/v1/pending','PENDING')
-INSERT  INTO permission_api_list(permission_id, api_list_id)
-values (17, 46)
+
 
  SET IDENTITY_INSERT url_api OFF
 
 END
+if(@checkPermPending = 0)
+BEGIN
+
+INSERT  INTO permission_api_list(permission_id, api_list_id)
+values (17, 46)
+
+END
 END;
+
+
+
 
 
 
