501de7d41109ef584cbee67c6f392e2b444afbc6
Elvin Shrestha <elwyncrestha@gmail.com>
2019-08-09 14:15:11 +0545

    Add Customer Controller, Specification

--- kb-api/src/main/java/com/sb/solutions/api/customer/repository/CustomerRepository.java
+++ kb-api/src/main/java/com/sb/solutions/api/customer/repository/CustomerRepository.java
@@ -1,16 +1,11 @@
 package com.sb.solutions.api.customer.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.sb.solutions.api.customer.entity.Customer;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-    @Query(value = "SELECT c FROM Customer c WHERE c.accountNo LIKE  CONCAT(:accountNo,'%')")
-    Page<Customer> customerFilter(@Param("accountNo") String accountNo, Pageable pageable);
+public interface CustomerRepository extends JpaRepository<Customer, Long>,
+    JpaSpecificationExecutor<Customer> {
 
 }
