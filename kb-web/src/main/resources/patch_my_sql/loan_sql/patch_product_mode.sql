69e41edaa6996de802400046fb5957f0e4fbfef1
yunish_stha <yunishshrestha8@gmail.com>
2019-08-18 13:39:25 +0545

    Query credit risk template in sql

--- kb-web/src/main/resources/patch_my_sql/loan_sql/patch_product_mode.sql
+++ kb-web/src/main/resources/patch_my_sql/loan_sql/patch_product_mode.sql
@@ -4,5 +4,5 @@ INSERT INTO `product_mode` (`id`, `product`,`status`,`description`)
   ('1', '0',1,'DMS'),
   ('2', '1',0,'MEMO'),
   ('3', '2',0,'ACCOUNT'),
-  ('4', '3',1,'ELIGIBILITY'),
-  ('5', '4',0,'LAS');
+  ('4', '3',0,'ELIGIBILITY'),
+  ('5', '4',1,'LAS');
