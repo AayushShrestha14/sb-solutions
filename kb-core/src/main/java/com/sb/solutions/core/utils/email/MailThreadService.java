cd5cd137f562223fb6bfb6118c6b81b5284b277e
rujnmahrzn <rujnmahrzn@gmail.com>
2019-08-26 15:54:50 -0700

    email configuration

--- kb-core/src/main/java/com/sb/solutions/core/utils/email/MailThreadService.java
+++ kb-core/src/main/java/com/sb/solutions/core/utils/email/MailThreadService.java
@@ -1,15 +1,15 @@
 package com.sb.solutions.core.utils.email;
 
-import java.io.IOException;
-import javax.mail.MessagingException;
-
+import com.sb.solutions.core.constant.EmailConstant;
+import com.sb.solutions.core.exception.ServiceValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.sb.solutions.core.constant.EmailConstant;
-import com.sb.solutions.core.exception.ServiceValidationException;
+import javax.mail.MessagingException;
+import java.io.IOException;
 
 @Service
 public class MailThreadService {
@@ -17,6 +17,12 @@ public class MailThreadService {
     private final Logger logger = LoggerFactory.getLogger(MailThreadService.class);
 
     private final MailSenderService mailSenderService;
+
+    @Value("${bank.name}")
+    private String bankName;
+
+    @Value("${bank.website}")
+    private String bankWebsite;
 
     public MailThreadService(@Autowired MailSenderService mailSenderService) {
 
@@ -57,7 +63,8 @@ public class MailThreadService {
     }
 
     public void testMail(EmailConstant.Template template, Email email) {
-
+        email.setBankName(bankName);
+        email.setBankWebsite(bankWebsite);
         mailSenderService.send(template, email);
 
 
