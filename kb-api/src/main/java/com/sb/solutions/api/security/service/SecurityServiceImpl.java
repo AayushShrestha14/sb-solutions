75a84bace72855f8ad869e357f191b70308529e9
SafiShrestha <safishrestha12@gmail.com>
2019-10-20 15:18:18 +0545

    security change set added

---
+++ kb-api/src/main/java/com/sb/solutions/api/security/service/SecurityServiceImpl.java
@@ -0,0 +1,46 @@
+package com.sb.solutions.api.security.service;
+
+import com.sb.solutions.api.security.entity.Security;
+import com.sb.solutions.api.security.repository.SecurityRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+@Service
+public class SecurityServiceImpl implements SecurityService {
+    final SecurityRepository securityRepository;
+
+    @Autowired
+    public SecurityServiceImpl(SecurityRepository securityRepository)
+    {
+        this.securityRepository = securityRepository;
+    }
+
+    @Override
+    public List<Security> findAll() {
+        return securityRepository.findAll();
+    }
+
+    @Override
+    public Security findOne(Long id) {
+        return securityRepository.getOne(id);
+    }
+
+    @Override
+    public Security save(Security security) {
+        return securityRepository.save(security);
+    }
+
+    @Override
+    public Page<Security> findAllPageable(Object t, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<Security> saveAll(List<Security> securityList) {
+        return securityRepository.saveAll(securityList);
+    }
+}
