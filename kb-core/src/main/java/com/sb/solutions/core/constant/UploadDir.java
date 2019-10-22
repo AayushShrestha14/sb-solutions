6fd77b96d0c08273e7bc00092189301b42114354
Elvin Shrestha <elwyncrestha@gmail.com>
2019-10-22 17:54:12 +0545

    FilePath from application properties

--- kb-core/src/main/java/com/sb/solutions/core/constant/UploadDir.java
+++ kb-core/src/main/java/com/sb/solutions/core/constant/UploadDir.java
@@ -1,12 +1,16 @@
 package com.sb.solutions.core.constant;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 /**
  * @author Rujan Maharjan on 4/3/2019
  */
+@Component
 public class UploadDir {
 
-    public static String WINDOWS_PATH = "C:/";
+    public static String WINDOWS_PATH;
     public static String Linux_PATH = "/var/";
     public static String branchCsv = "images/branchCsv";
     public static String userCsv = "images/userCsv";
@@ -14,4 +18,9 @@ public class UploadDir {
     public static String userProfile = "images/userProfile/";
     public static String userSignature = "images/userSignature/";
     public static String initialDocument = "images/";
+
+    @Value("${file.upload-directory}")
+    public void setWindowsPath(String windowsPath) {
+        WINDOWS_PATH = windowsPath;
+    }
 }
