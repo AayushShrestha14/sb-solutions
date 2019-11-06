2e5c06b35b4c6075ec22d370ff2758e1d092983e
yunish_stha <yunishshrestha8@gmail.com>
2019-11-06 17:20:18 +0545

    add dto, refactor changeset

---
+++ kb-api/src/main/java/com/sb/solutions/api/loan/dto/CustomerDocumentDto.java
@@ -0,0 +1,10 @@
+package com.sb.solutions.api.loan.dto;
+
+import com.sb.solutions.core.dto.BaseDto;
+
+/**
+ * @author yunish on 11/6/2019
+ */
+public class CustomerDocumentDto extends BaseDto<Long> {
+
+}
