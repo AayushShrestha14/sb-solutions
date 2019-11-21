85a076a6b83f75528db4b508706108674a657562
123nishan <30835894+123nishan@users.noreply.github.com>
2019-11-21 12:39:40 +0545

    dms and las combo

--- kb-web/src/main/resources/patch_ms_sql/loan_sql/patch_product_mode.sql
+++ kb-web/src/main/resources/patch_ms_sql/loan_sql/patch_product_mode.sql
@@ -2,11 +2,11 @@ SET IDENTITY_INSERT product_mode ON;
 TRUNCATE table product_mode;
 INSERT INTO product_mode (id, product,status,description)
 VALUES
-('1', '0',1,'DMS'),
+('1', '0',0,'DMS'),
 ('2', '1',0,'MEMO'),
 ('3', '2',1,'ACCOUNT'),
 ('4', '3',0,'ELIGIBILITY'),
-('5', '4',0,'LAS'),
+('5', '4',1,'LAS'),
 ('6', '5',0,'OFFER LETTER');
 
 SET IDENTITY_INSERT product_mode OFF;
