aa14b6ecf1373d94c88e2bc5e780debdb65c6c95
Elvin Shrestha <elwyncrestha@gmail.com>
2019-12-11 18:00:44 +0545

    Feat: User Revision/Audit APIs
    Added User Revision API to display all revisions.

---
+++ kb-api/src/main/java/com/sb/solutions/api/user/repository/UserRevisionRepository.java
@@ -0,0 +1,8 @@
+package com.sb.solutions.api.user.repository;
+
+/**
+ * @author Elvin Shrestha on 11/12/2019
+ */
+public class UserRevisionRepository {
+
+}
---
+++ kb-web/src/main/java/com/sb/solutions/web/user/UserRevisionController.java
@@ -0,0 +1,8 @@
+package com.sb.solutions.web.user;
+
+/**
+ * @author Elvin Shrestha on 11/12/2019
+ */
+public class UserRevisionController {
+
+}
