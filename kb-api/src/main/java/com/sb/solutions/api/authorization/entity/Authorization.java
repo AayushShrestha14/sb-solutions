443a894675973d34458d7f188c1ef20d4c4e890d
sushil <np.msushil@gmail.com>
2019-12-30 23:20:36 +0545

    Add Authorization entity

---
+++ kb-api/src/main/java/com/sb/solutions/api/authorization/entity/Authorization.java
@@ -0,0 +1,24 @@
+package com.sb.solutions.api.authorization.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import com.sb.solutions.core.enitity.BaseEntity;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Authorization extends BaseEntity<Long> implements Serializable {
+
+    private String name;
+    private String value;
+    private String key;
+}
