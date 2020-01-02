e06938257c3618a43c15d4b9568bdb8b05a36a1b
buddha.stha04 <buddha.stha04@gmail.com>
2020-01-02 16:03:24 +0545

    pipeline fix

--- kb-api/src/main/java/com/sb/solutions/api/group/service/GroupServices.java
+++ kb-api/src/main/java/com/sb/solutions/api/group/service/GroupServices.java
@@ -1,7 +1,5 @@
 package com.sb.solutions.api.group.service;
 
-import org.springframework.stereotype.Service;
-
 import com.sb.solutions.api.group.entity.Group;
 import com.sb.solutions.core.service.BaseService;
 
