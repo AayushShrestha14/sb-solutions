ceb2cc2d8fced3c7313e3885a5cf5c53edf47ef4
sunilbau <whispersunil@gmail.com>
2020-01-16 15:40:22 +0545

    modification done for share security excel upload

--- kb-api/src/main/java/com/sb/solutions/api/nepseCompany/util/ExcelReader.java
+++ kb-api/src/main/java/com/sb/solutions/api/nepseCompany/util/ExcelReader.java
@@ -1,70 +1,81 @@
 package com.sb.solutions.api.nepseCompany.util;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.sb.solutions.api.nepseCompany.entity.NepseCompany;
 import com.sb.solutions.core.enums.ShareType;
 import com.sb.solutions.core.exception.ServiceValidationException;
 import com.sb.solutions.core.utils.csv.ExcelHeaderChecker;
 import com.sb.solutions.core.utils.file.FileUploadUtils;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 @Component
 public class ExcelReader {
 
     private static final Logger logger = LoggerFactory.getLogger(FileUploadUtils.class);
+    private static final int COMPANY_NAME_COLUMN_POS = 0;
+    private static final int SHARE_AMOUNT_COLUMN_POS = 1;
+    private static final int COMPANY_CODE_COLUMN_POS = 2;
+    private static final int SHARE_TYPE_COLUMN_POS = 3;
     private ExcelHeaderChecker excelHeaderChecker = new ExcelHeaderChecker();
 
     public List<NepseCompany> parseNepseCompanyFile(MultipartFile multipartFile) {
         List<NepseCompany> nepseCompanyList = new ArrayList<>();
-        HSSFWorkbook workbook;
+        Workbook wb = null;
+
         try {
-            workbook = new HSSFWorkbook(multipartFile.getInputStream());
+            wb = WorkbookFactory.create(multipartFile.getInputStream());
+
+            Sheet workSheet = wb.getSheetAt(0);
+            int i = 0;
+            IntStream.rangeClosed(1, workSheet.getLastRowNum()).forEach(value -> {
+                Row row = workSheet.getRow(value);
+                NepseCompany nepseCompany = new NepseCompany();
+
+                if (row.getCell(COMPANY_NAME_COLUMN_POS).toString() != null) {
+                    nepseCompany.setCompanyName(row.getCell(COMPANY_NAME_COLUMN_POS).getStringCellValue());
+                }
+                if (row.getCell(SHARE_AMOUNT_COLUMN_POS).toString() != null) {
+                    nepseCompany.setAmountPerUnit(row.getCell(SHARE_AMOUNT_COLUMN_POS).getNumericCellValue());
+                }
+                if (row.getCell(COMPANY_CODE_COLUMN_POS).toString() != null) {
+                    nepseCompany.setCompanyCode(row.getCell(COMPANY_CODE_COLUMN_POS).toString());
+                }
+                if (row.getCell(SHARE_TYPE_COLUMN_POS).toString() != null) {
+                    if (!(row.getCell(SHARE_TYPE_COLUMN_POS).toString().trim().equalsIgnoreCase(ShareType.ORDINARY.toString())
+                            || row.getCell(SHARE_TYPE_COLUMN_POS).toString().trim().equalsIgnoreCase(ShareType.PROMOTER.toString()))) {
+                        throw new ServiceValidationException(row.getCell(SHARE_TYPE_COLUMN_POS).toString() + " "
+                                + "is  not valid ShareType on row" + " " + (value - 1));
+                    }
+                    nepseCompany.setShareType(
+                            ShareType.valueOf(row.getCell(3).getStringCellValue().toUpperCase()));
+                }
+                nepseCompanyList.add(nepseCompany);
+            });
         } catch (Exception e) {
             logger.error("invalid file format {}", e.getMessage());
             throw new ServiceValidationException("File format is not valid");
-        }
-        HSSFSheet workSheet = workbook.getSheetAt(0);
-        System.out.println(workSheet.getLastRowNum());
-        int i = 0;
-        while (i <= workSheet.getLastRowNum()) {
-            HSSFRow row = workSheet.getRow(i++);
-            if (i == 1) {
-                excelHeaderChecker.checkNepseHeader(row);
-            } else if (i > 1) {
-                NepseCompany nepseCompany = new NepseCompany();
-                if (row.getCell(0).toString() != null) {
-                    nepseCompany.setCompanyName(row.getCell(0).getStringCellValue());
+        } finally {
+            if (null != wb) {
+                try {
+                    wb.close();
+                } catch (IOException e) {
+                    logger.error("Failed to close poi workbook {}", e);
                 }
-                if (row.getCell(1).toString() != null) {
-                    nepseCompany.setAmountPerUnit(row.getCell(1).getNumericCellValue());
-                }
-                if (row.getCell(2).toString() != null) {
-                    nepseCompany.setCompanyCode(row.getCell(2).toString());
-                }
-                if (row.getCell(3).toString() != null) {
-                    if (!(row.getCell(3).toString().equalsIgnoreCase(ShareType.ORDINARY.toString())
-                        || row.getCell(3).toString().equalsIgnoreCase(ShareType.PROMOTER.toString()))) {
-                        throw new ServiceValidationException(row.getCell(3).toString() + " "
-                            + "is  not valid ShareType on row" + " " + (i - 1));
-                    }
-                    nepseCompany.setShareType(
-                        ShareType.valueOf(row.getCell(3).getStringCellValue().toUpperCase()));
-                }
-                nepseCompanyList.add(nepseCompany);
             }
         }
-
         return nepseDataValidator(nepseCompanyList);
     }
 
@@ -74,16 +85,16 @@ public class ExcelReader {
         nepseCompanies.forEach(nepseCompany -> {
             if (nepseCompany.getShareType().equals(ShareType.ORDINARY)) {
                 if (nepseList.get(nepseCompany.getCompanyName()) != null
-                    && nepseList.get(nepseCompany.getCompanyName()).getShareType()
-                    .equals(ShareType.ORDINARY)) {
+                        && nepseList.get(nepseCompany.getCompanyName()).getShareType()
+                        .equals(ShareType.ORDINARY)) {
                     return;
                 }
                 nepseList
-                    .put(nepseCompany.getCompanyName(), nepseCompany);
+                        .put(nepseCompany.getCompanyName(), nepseCompany);
             } else {
                 if (nepseList.get(nepseCompany.getCompanyName()) != null
-                    && nepseList.get(nepseCompany.getCompanyName()).getShareType()
-                    .equals(ShareType.PROMOTER)) {
+                        && nepseList.get(nepseCompany.getCompanyName()).getShareType()
+                        .equals(ShareType.PROMOTER)) {
                     return;
                 }
                 nepseList.put(nepseCompany.getCompanyName(), nepseCompany);
--- kb-core/src/main/java/com/sb/solutions/core/utils/csv/ExcelHeaderChecker.java
+++ kb-core/src/main/java/com/sb/solutions/core/utils/csv/ExcelHeaderChecker.java
@@ -1,16 +1,15 @@
 package com.sb.solutions.core.utils.csv;
 
+import com.sb.solutions.core.exception.ServiceValidationException;
+import org.apache.poi.ss.usermodel.Row;
+
 import java.util.Arrays;
 
-import org.apache.poi.hssf.usermodel.HSSFRow;
-
-import com.sb.solutions.core.exception.ServiceValidationException;
-
 public class ExcelHeaderChecker {
-    public void checkNepseHeader(HSSFRow row) {
+    public void checkNepseHeader(Row row) {
         row.forEach(c -> {
             if (!(Arrays.asList("companyname", "value", "companycode", "sharetype")
-                .contains(c.getStringCellValue().replaceAll("\\s","").toLowerCase()))) {
+                    .contains(c.getStringCellValue().replaceAll("\\s", "").toLowerCase()))) {
                 throw new ServiceValidationException(c.getStringCellValue() + " " + "is not valid Header");
             }
         });
