0ce081cde3cd1f0af7d9547a36e5feb5991a0be6
buddha.stha04 <buddha.stha04@gmail.com>
2020-01-21 13:35:05 +0545

    groups_valuator third table

--- kb-api/src/main/java/com/sb/solutions/api/group/entity/Group.java
+++ kb-api/src/main/java/com/sb/solutions/api/group/entity/Group.java
@@ -1,11 +1,14 @@
 package com.sb.solutions.api.group.entity;
 
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import com.sb.solutions.api.valuator.entity.Valuator;
 import com.sb.solutions.core.enitity.BaseEntity;
 
 @Data
@@ -15,5 +18,8 @@ import com.sb.solutions.core.enitity.BaseEntity;
 public class Group extends BaseEntity<Long> {
 
     private String data;
+
+    @ManyToMany
+    private List<Valuator> valuator;
 
 }
