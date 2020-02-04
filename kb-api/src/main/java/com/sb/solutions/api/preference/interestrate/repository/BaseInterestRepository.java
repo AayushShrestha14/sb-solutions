755154a397306e26ea28dc751e3dcde7b4ed2c4c
Bibash7 <biswash166@gmail.com>
2020-02-04 14:08:41 +0545

    displaying base rate in proposal tab

--- kb-api/src/main/java/com/sb/solutions/api/preference/interestrate/repository/BaseInterestRepository.java
+++ kb-api/src/main/java/com/sb/solutions/api/preference/interestrate/repository/BaseInterestRepository.java
@@ -2,7 +2,6 @@ package com.sb.solutions.api.preference.interestrate.repository;
 import com.sb.solutions.api.preference.interestrate.entity.BaseInterestRate;
 import com.sb.solutions.core.enums.Status;
 
-import org.codehaus.jackson.map.Serializers.Base;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
