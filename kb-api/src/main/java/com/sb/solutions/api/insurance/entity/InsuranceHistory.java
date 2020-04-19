f78b1e4f62e3b188d281d12874f7c39cb98fb793
Elvin Shrestha <elwyncrestha@gmail.com>
2020-04-19 17:44:43 +0545

    Feature: Added API for updating Insurance

--- kb-api/src/main/java/com/sb/solutions/api/insurance/entity/InsuranceHistory.java
+++ kb-api/src/main/java/com/sb/solutions/api/insurance/entity/InsuranceHistory.java
@@ -7,34 +7,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import com.sb.solutions.core.enitity.BaseEntity;
 
 /**
- * @author Aashish shrestha on 6th. March, 2020
+ * @author Elvin Shrestha on 4/19/2020
  */
-
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class Insurance extends BaseEntity<Long> {
+public class InsuranceHistory extends BaseEntity<Long> {
 
+    private Long customerLoanId;
     private String company;
-
     private double insuredAmount;
-
     private double premiumAmount;
-
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date issuedDate;
-
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date expiryDate;
-
     private String policyType;
-
-    private String remarks;
 }
