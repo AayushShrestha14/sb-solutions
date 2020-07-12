74278e17ccec71027a7c78583ecc2e8a26444da0
noitanec <ynoitanec@yahoo.com>
2020-07-12 13:03:30 +0545

    refactor: Refactor for matching citizenship

--- kb-api/src/main/java/com/sb/solutions/api/customer/service/CustomerService.java
+++ kb-api/src/main/java/com/sb/solutions/api/customer/service/CustomerService.java
@@ -1,13 +1,14 @@
 package com.sb.solutions.api.customer.service;
 
 import java.util.Date;
+import java.util.List;
 
 import com.sb.solutions.api.customer.entity.Customer;
 import com.sb.solutions.core.service.BaseService;
 
 public interface CustomerService extends BaseService<Customer> {
 
-    Customer findCustomerByCitizenshipNumber(String citizenshipNumber);
+    List<Customer> findCustomerByCitizenshipNumber(String citizenshipNumber);
 
     Customer findCustomerByCustomerNameAndCitizenshipNumberAndCitizenshipIssuedDate(String name,
         String citizenship,
