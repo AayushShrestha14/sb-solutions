7c0fea13629449ee1f7c2d589ef3a2e677265fe9
Rujan Maharjan <rujnmahrzn@gmail.com>
2020-08-22 12:40:13 +0545

    changed Util date to local Date

--- kb-core/src/main/java/com/sb/solutions/core/utils/date/Converter.java
+++ kb-core/src/main/java/com/sb/solutions/core/utils/date/Converter.java
@@ -1,5 +1,6 @@
 package com.sb.solutions.core.utils.date;
 
+import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -15,15 +16,14 @@ import lombok.NoArgsConstructor;
 public class Converter {
 
     //Change this when you get more previous database.
-    public static final int START_ENGLISH_YEAR = 1953;
+    public static final int START_ENGLISH_YEAR = 1943;
     public static final int START_ENGLISH_MONTH = 4;
-    public static final int START_ENGLISH_DAY = 13;
+    public static final int START_ENGLISH_DAY = 14;
 
     //Change this to equivalent nepali start date
-    public static final int START_NEPALI_YEAR = 2010;
+    public static final int START_NEPALI_YEAR = 2000;
     public static final int START_NEPALI_MONTH = 1;
     public static final int START_NEPALI_DAY = 1;
-
 
     /*
         English units start with e
@@ -34,13 +34,14 @@ public class Converter {
         int nMonth = START_NEPALI_MONTH;
         int nYear = START_NEPALI_YEAR;
 
-        final Calendar start = Calendar.getInstance();
-        start.set(START_ENGLISH_YEAR, START_ENGLISH_MONTH, START_ENGLISH_DAY);
-
         final Calendar end = Calendar.getInstance();
         end.set(eYear, eMonth, eDay);
-        int tempDeltaDays =
-            Math.toIntExact(ChronoUnit.DAYS.between(start.toInstant(), end.toInstant()));
+
+        LocalDate startDate = LocalDate
+            .of(START_ENGLISH_YEAR, START_ENGLISH_MONTH, START_ENGLISH_DAY);
+        LocalDate endDate = LocalDate.of(eYear, eMonth, eDay);
+
+        int tempDeltaDays = (int) ChronoUnit.DAYS.between(startDate, endDate);
         NepaliDate nepaliDate = new NepaliDate();
         for (int i = 0; i < tempDeltaDays; i++) {
             if (nDay < nepaliDate.getDaysOf(nYear, nMonth)) {
@@ -59,36 +60,6 @@ public class Converter {
 
         return new NepaliDate(nYear, nMonth, nDay, weekDay);
     }
-
-    /*public com.sb.solutions.core.utils.NepaliDateConvertor.EnglishDate
-     getEnglishDate(int nYear, int nMonth, int nDay) {
-        int l_day = START_NEPALI_DAY, l_month = START_NEPALI_MONTH, l_year = START_NEPALI_YEAR;
-        int deltaDays = 0;
-        boolean isReached = false;
-        NepaliDate nepaliDate = new NepaliDate();
-        while (!isReached) {
-            if (nYear == l_year && nMonth == l_month && nDay == l_day) {
-                isReached = true;
-                deltaDays--;
-            }
-            deltaDays++;
-            if (l_day < nepaliDate.getDaysOf(l_year, l_month)) {
-                l_day++;
-            } else if (l_month < 12) {
-                l_day = 1;
-                l_month++;
-            } else if (l_month == 12) {
-                l_year++;
-                l_month = 1;
-                l_day = 1;
-                System.out.println();
-            }
-        }
-
-        Date Date = new Date(START_ENGLISH_YEAR, START_ENGLISH_MONTH, START_ENGLISH_DAY, 0 , 0);
-        return new com.sb.solutions.core.utils.NepaliDateConvertor.
-        EnglishDate(Date.withFieldAdded(DurationFieldType.days(), deltaDays));
-    }*/
 
     public List<NepaliDate> getFullNepaliMonthOf(int eYear, int eMonth, int eDay) {
 
