180c2ed93092f93ab974f968b89fb6e3e00cc20f
noitanec <ynoitanec@yahoo.com>
2020-08-23 19:50:22 +0545

    Fix date format

--- kb-api/src/main/java/com/sb/solutions/api/guarantor/entity/Guarantor.java
+++ kb-api/src/main/java/com/sb/solutions/api/guarantor/entity/Guarantor.java
@@ -42,7 +42,7 @@ public class Guarantor extends BaseEntity<Long> {
 
     private String citizenNumber;
 
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd")
     private Date issuedYear;
 
     private String issuedPlace;
