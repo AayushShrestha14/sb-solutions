6b1d83d9d32af4beb205fc9d376b7ab131e1c7ac
Bibash7 <biswash166@gmail.com>
2020-08-24 11:50:18 +0545

    fixed pipeline

--- kb-core/src/main/java/com/sb/solutions/core/constant/AppConstant.java
+++ kb-core/src/main/java/com/sb/solutions/core/constant/AppConstant.java
@@ -8,7 +8,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class AppConstant {
 
-        public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final String MM_DD_YYYY = "MM-dd-yyyy";
     // List available template id
     public static final int TEMPLATE_SHARE_SECURITY = 13;
