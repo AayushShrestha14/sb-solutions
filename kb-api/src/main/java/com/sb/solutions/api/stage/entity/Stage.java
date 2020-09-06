051c2f31644b70a8a2bfc51041cfc0891aa06039
Rujan Maharjan <rujnmahrzn@gmail.com>
2020-09-06 14:05:47 +0545

    added Word Formatter in comment of loan Stage

--- kb-api/src/main/java/com/sb/solutions/api/stage/entity/Stage.java
+++ kb-api/src/main/java/com/sb/solutions/api/stage/entity/Stage.java
@@ -3,6 +3,7 @@ package com.sb.solutions.api.stage.entity;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.envers.Audited;
@@ -11,6 +12,7 @@ import com.sb.solutions.api.authorization.entity.Role;
 import com.sb.solutions.api.user.entity.User;
 import com.sb.solutions.core.enitity.BaseEntity;
 import com.sb.solutions.core.enums.DocAction;
+import com.sb.solutions.core.utils.string.WordFormatter;
 
 /**
  * @author Sunil Babu Shrestha on 5/24/2019
@@ -35,5 +37,6 @@ public class Stage extends BaseEntity<Long> {
 
     private DocAction docAction;
 
+    @JsonDeserialize(using = WordFormatter.class)
     private String comment;
 }
