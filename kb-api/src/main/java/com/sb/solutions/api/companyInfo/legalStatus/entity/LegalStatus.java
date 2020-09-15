60337d80489f9fecfda2836b483c8948b119a901
buddha.stha04 <buddha.stha04@gmail.com>
2020-09-15 15:25:34 +0545

    added corporate structure as a onetoone foreign key of company table in legal status

--- kb-api/src/main/java/com/sb/solutions/api/companyInfo/legalStatus/entity/LegalStatus.java
+++ kb-api/src/main/java/com/sb/solutions/api/companyInfo/legalStatus/entity/LegalStatus.java
@@ -5,15 +5,18 @@ import java.util.Objects;
 import java.util.stream.Stream;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.hibernate.envers.Audited;
+import org.hibernate.envers.RelationTargetAuditMode;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.util.Pair;
 
+import com.sb.solutions.api.company.entity.Company;
 import com.sb.solutions.core.enitity.BaseEntity;
 import com.sb.solutions.core.enitity.EntityValidator;
 
@@ -26,7 +29,9 @@ import com.sb.solutions.core.enitity.EntityValidator;
 @Audited
 public class LegalStatus extends BaseEntity<Long> implements EntityValidator {
 
-    private String corporateStructure;
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    @OneToOne
+    private Company corporateStructure;
 
     private String registeredOffice;
 
