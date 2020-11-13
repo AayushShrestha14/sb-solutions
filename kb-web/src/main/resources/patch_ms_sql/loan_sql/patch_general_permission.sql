a707c600aef4cd722e3219ac5c47c4684b22531c
lucifer403403 <np01cp4a170169@islingtoncollege.edu.np>
2020-11-13 12:55:05 +0545

    update

--- kb-web/src/main/resources/patch_ms_sql/loan_sql/patch_general_permission.sql
+++ kb-web/src/main/resources/patch_ms_sql/loan_sql/patch_general_permission.sql
@@ -3,6 +3,8 @@ BEGIN
     DECLARE
         @count smallint
     DECLARE
+        @countapproval smallint
+    DECLARE
         @countPull smallint
     DECLARE
         @count_preference smallint
@@ -12,9 +14,9 @@ BEGIN
     SET @countPull = (Select count(*) from permission where id = 125)
     SET @count_preference = (SELECT count(*) from permission where id = 142)
     SET @count_customer = (SELECT count(*) from permission where id = 143)
+    SET @countapproval = (Select count(*) from permission where id = 150)
 
-
-    if (@count < 1)
+    if (@count < 8)
         BEGIN
             SET IDENTITY_INSERT permission ON
             INSERT INTO permission (id, permission_name, fa_icon, front_url, orders, status)
@@ -31,8 +33,6 @@ BEGIN
             VALUES (17, 'Dashboard', 'home-outline', '/home/admin/dashboard', 1, 1)
             INSERT INTO permission (id, permission_name, fa_icon, front_url, orders, status)
             VALUES (102, 'Catalogue', 'layers-outline', '/home/admin/catalogue', 14, 1)
-            INSERT INTO permission (id, permission_name, fa_icon, front_url, orders, status)
-            VALUES (150, 'Post Approval', 'checkmark-circle-outline', '/home/loan/post-approval-form', 110, 1)
             SET IDENTITY_INSERT permission OFF
 
 
@@ -195,7 +195,12 @@ BEGIN
             SET IDENTITY_INSERT role_permission_rights Off
         end
 
-
+        if(@countapproval < 1)
+        BEGIN
+        SET IDENTITY_INSERT permission ON
+            INSERT INTO permission (id, permission_name, fa_icon, front_url, orders, status)
+            VALUES (150, 'Post Approval', 'checkmark-circle-outline', '/home/loan/post-approval-form', 110, 1)
+        end
 END;
 
 
