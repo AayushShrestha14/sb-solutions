6c180d887ac0d1d5806a7173c085221212e69c64
buddha.stha04 <buddha.stha04@gmail.com>
2020-11-26 18:32:44 +0545

    business given

--- kb-api/src/main/java/com/sb/solutions/api/companyInfo/model/entity/CompanyInfo.java
+++ kb-api/src/main/java/com/sb/solutions/api/companyInfo/model/entity/CompanyInfo.java
@@ -97,6 +97,9 @@ public class CompanyInfo extends BaseEntity<Long> implements EntityValidator {
     @Transient
     private String subsectorDetail;
 
+    @NotAudited
+    private String businessGiven;
+
     @Override
     public Pair<Boolean, String> valid() {
         Boolean validator = Boolean.TRUE;
