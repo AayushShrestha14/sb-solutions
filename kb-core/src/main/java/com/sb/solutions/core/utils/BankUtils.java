8cfa3cc255b09a751e239a72d652032bf7a7d287
rujnmahrzn <rujnmahrzn@gmail.com>
2020-12-07 14:42:45 +0545

    updated jacoco-plugin 0.8.4

--- kb-core/src/main/java/com/sb/solutions/core/utils/BankUtils.java
+++ kb-core/src/main/java/com/sb/solutions/core/utils/BankUtils.java
@@ -6,16 +6,18 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 
 public class BankUtils {
+
     public static String AFFILIATED_ID;
 
-    public static Map<String,Object> map = new HashMap<>();
+    public static Map<String, Object> map = new HashMap<>();
 
     @Value("${bank.affiliateId}")
     public void setAffiliatedId(String affiliatedId) {
         AFFILIATED_ID = affiliatedId;
-        map.put("AFFILIATED_ID",affiliatedId);
+        map.put("AFFILIATED_ID", affiliatedId);
     }
 
-    public static Map<String,Object> getMap() {
-        return map;}
+    public static Map<String, Object> getMap() {
+        return map;
+    }
 }
