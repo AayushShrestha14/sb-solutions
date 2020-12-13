3c0e3983beec507a6ed5577e5b9b283520d4f9b8
sunilbau <whispersunil@gmail.com>
2020-12-13 12:19:52 +0545

    add customer and proposal limit on bell notification

--- kb-api/src/main/java/com/sb/solutions/api/userNotification/service/MessageServiceImpl.java
+++ kb-api/src/main/java/com/sb/solutions/api/userNotification/service/MessageServiceImpl.java
@@ -3,8 +3,8 @@ package com.sb.solutions.api.userNotification.service;
 import java.util.List;
 import java.util.Map;
 
+import lombok.RequiredArgsConstructor;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import com.sb.solutions.api.authorization.entity.Role;
 import com.sb.solutions.api.authorization.service.RoleService;
+import com.sb.solutions.api.loan.entity.CustomerLoan;
+import com.sb.solutions.api.loan.service.CustomerLoanService;
 import com.sb.solutions.api.user.entity.User;
 import com.sb.solutions.api.user.service.UserService;
 import com.sb.solutions.api.userNotification.entity.Message;
@@ -21,22 +23,14 @@ import com.sb.solutions.core.enums.DocAction;
 import com.sb.solutions.core.enums.Status;
 
 @Service
+@RequiredArgsConstructor
 public class MessageServiceImpl implements MessageService {
 
     private final MessageRepository messageRepository;
     private final UserService userService;
     private final RoleService roleService;
+    private final CustomerLoanService customerLoanService;
 
-    @Autowired
-    public MessageServiceImpl(
-        MessageRepository messageRepository,
-        UserService userService,
-        RoleService roleService
-    ) {
-        this.messageRepository = messageRepository;
-        this.userService = userService;
-        this.roleService = roleService;
-    }
 
     @Override
     public List<Message> findAll() {
@@ -57,23 +51,30 @@ public class MessageServiceImpl implements MessageService {
             message.setStatus(Status.ACTIVE);
             User toUser = userService.findOne(message.getToUserId());
             Role toRole = roleService.findOne(message.getToRoleId());
+            CustomerLoan customerLoan = customerLoanService.findOne(message.getCustomerId());
 
             if (message.getDocAction().equals(DocAction.FORWARD)
                 || message.getDocAction().equals(DocAction.BACKWARD)
                 || message.getDocAction().equals(DocAction.TRANSFER)) {
-                message.setMessage(String.format("%s (%s) has %s a loan document to %s (%s).",
+                message.setMessage(String.format(
+                    "%s (%s) has %s a loan document of Customer %s of limit Rs. %s to %s (%s).",
                     fromUser.getUsername(),
                     fromRole.getRoleName(),
                     message.getDocAction().toString().toLowerCase(),
+                    customerLoan.getCustomerInfo().getCustomerName(),
+                    customerLoan.getProposal().getProposedLimit(),
                     toUser.getUsername(),
                     toRole.getRoleName()));
             } else if (message.getDocAction().equals(DocAction.APPROVED)
                 || message.getDocAction().equals(DocAction.REJECT)
                 || message.getDocAction().equals(DocAction.CLOSED)) {
-                message.setMessage(String.format("%s (%s) has %s a loan document.",
-                    fromUser.getUsername(),
-                    fromRole.getRoleName(),
-                    message.getDocAction().toString().toLowerCase()));
+                message.setMessage(
+                    String.format("%s (%s) has %s a loan document of Customer %s of limit Rs. %s.",
+                        fromUser.getUsername(),
+                        fromRole.getRoleName(),
+                        message.getDocAction().toString().toLowerCase(),
+                        customerLoan.getCustomerInfo().getCustomerName(),
+                        customerLoan.getProposal().getProposedLimit()));
             }
         }
         return messageRepository.save(message);
