42215d7a3b061dcfd5f6f47093aede89e511d1fa
rujnmahrzn <rujnmahrzn@gmail.com>
2020-12-23 13:31:01 +0545

    added column in customer info

--- kb-api/src/main/java/com/sb/solutions/api/customer/repository/CustomerInfoRepository.java
+++ kb-api/src/main/java/com/sb/solutions/api/customer/repository/CustomerInfoRepository.java
@@ -2,6 +2,11 @@ package com.sb.solutions.api.customer.repository;
 
 import java.util.Date;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.sb.solutions.api.customer.entity.CustomerInfo;
 import com.sb.solutions.api.customer.enums.CustomerIdType;
 import com.sb.solutions.api.customer.enums.CustomerType;
@@ -17,4 +22,9 @@ public interface CustomerInfoRepository extends BaseRepository<CustomerInfo, Lon
     CustomerInfo findByCustomerTypeAndIdNumberAndIdRegPlaceAndIdTypeAndIdRegDate(
         CustomerType customerType, String idNumber, String idRegPlace,
         CustomerIdType customerIdType, Date date);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE CustomerInfo  c SET c.obligor=:obligor WHERE c.id=:id")
+    void updateObligorByCustomerInfoId(@Param("obligor")String obligor,@Param("id")Long id);
 }
