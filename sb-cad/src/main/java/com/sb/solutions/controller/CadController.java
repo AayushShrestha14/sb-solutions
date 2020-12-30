94e5f025e434b3eef6a09b14150f48d8f43e12dd
rujnmahrzn <rujnmahrzn@gmail.com>
2020-12-30 14:23:12 +0545

    api for cad role Hierarchy

--- sb-cad/src/main/java/com/sb/solutions/controller/CadController.java
+++ sb-cad/src/main/java/com/sb/solutions/controller/CadController.java
@@ -1,19 +1,23 @@
 package com.sb.solutions.controller;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sb.solutions.api.authorization.approval.ApprovalRoleHierarchyService;
 import com.sb.solutions.constant.ApiConstants;
-import com.sb.solutions.constant.ValidateConstants;
 import com.sb.solutions.core.dto.RestResponseDto;
+import com.sb.solutions.core.utils.ApprovalType;
 import com.sb.solutions.core.utils.PaginationUtils;
-import com.sb.solutions.core.validation.constraint.SbValid;
 import com.sb.solutions.dto.CadStageDto;
 import com.sb.solutions.service.LoanHolderService;
 import com.sb.solutions.validation.constraint.CadValid;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 
 /**
@@ -25,9 +29,12 @@ import java.util.Map;
 public class CadController {
 
     private final LoanHolderService loanHolderService;
+    private final ApprovalRoleHierarchyService approvalRoleHierarchyService;
 
-    public CadController(LoanHolderService loanHolderService) {
+    public CadController(LoanHolderService loanHolderService,
+        ApprovalRoleHierarchyService approvalRoleHierarchyService) {
         this.loanHolderService = loanHolderService;
+        this.approvalRoleHierarchyService = approvalRoleHierarchyService;
     }
 
     @PostMapping(value = ApiConstants.UNASSIGNED_LOAN)
@@ -61,7 +68,6 @@ public class CadController {
     }
 
 
-
     @PostMapping(value = ApiConstants.ASSIGN_LOAN_TO_USER)
     public ResponseEntity<?> assignLoanToUser(@RequestBody CadStageDto cadStageDto) {
         return new RestResponseDto().successModel(loanHolderService.assignLoanToUser(cadStageDto));
@@ -72,6 +78,13 @@ public class CadController {
     @PostMapping(value = ApiConstants.CAD_ACTION)
     public ResponseEntity<?> action(@RequestBody CadStageDto cadStageDto) {
         return new RestResponseDto().successModel(loanHolderService.cadAction(cadStageDto));
+    }
+
+    @GetMapping(value = "/cad-role-list")
+    public ResponseEntity<?> getRoleListPresentInCAD() {
+        return new RestResponseDto()
+            .successModel(
+                approvalRoleHierarchyService.getRoles(ApprovalType.CAD, 0L));
     }
 
 }
