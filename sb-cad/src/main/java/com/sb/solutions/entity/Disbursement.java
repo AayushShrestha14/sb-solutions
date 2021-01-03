a31eaf3769b5878194d2b30bd1bb91bc2b687883
rujnmahrzn <rujnmahrzn@gmail.com>
2021-01-03 17:21:05 +0545

    exposure init

---
+++ sb-cad/src/main/java/com/sb/solutions/entity/Disbursement.java
@@ -0,0 +1,28 @@
+//package com.sb.solutions.entity;
+//
+//import java.util.List;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.OneToMany;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//import lombok.NoArgsConstructor;
+//
+//import com.sb.solutions.core.enitity.BaseEntity;
+//
+///**
+// * @author : Rujan Maharjan on  1/3/2021
+// **/
+//@Entity
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@EqualsAndHashCode(callSuper = true)
+//public class Disbursement extends BaseEntity<Long> {
+//
+//    @OneToMany
+//    private List<DisbursementData>  disbursementData;
+//
+//}
