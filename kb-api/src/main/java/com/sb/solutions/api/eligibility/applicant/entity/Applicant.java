48c626cdbdaa8207988fc1463bb686e3e3dc3be5
Amulya Stha <sthaamulya@gmail.com>
2021-01-10 22:44:29 +0545

    409 issues resolve

--- kb-api/src/main/java/com/sb/solutions/api/eligibility/applicant/entity/Applicant.java
+++ kb-api/src/main/java/com/sb/solutions/api/eligibility/applicant/entity/Applicant.java
@@ -1,6 +1,7 @@
 package com.sb.solutions.api.eligibility.applicant.entity;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
@@ -35,6 +36,8 @@ public class Applicant extends BaseEntity<Long> {
     private String fullName;
 
     private long age;
+
+    private Date dob;
 
     private long phoneNumber;
 
