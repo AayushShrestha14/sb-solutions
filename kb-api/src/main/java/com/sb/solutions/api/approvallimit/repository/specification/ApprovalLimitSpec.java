ff3ee8247569503123f66e512ab2275b93cd1340
sunilbau <whispersunil@gmail.com>
2021-02-24 15:32:29 +0545

    SRDB #33 Filter implementation on Approval Limit

--- kb-api/src/main/java/com/sb/solutions/api/approvallimit/repository/specification/ApprovalLimitSpec.java
+++ kb-api/src/main/java/com/sb/solutions/api/approvallimit/repository/specification/ApprovalLimitSpec.java
@@ -1,5 +1,6 @@
 package com.sb.solutions.api.approvallimit.repository.specification;
 
+import com.sb.solutions.api.approvallimit.emuns.LoanApprovalType;
 import com.sb.solutions.api.approvallimit.entity.ApprovalLimit;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -8,6 +9,9 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+/**
+ * @author Sunil Babu Shrestha on 2/24/2021
+ */
 public class ApprovalLimitSpec implements Specification<ApprovalLimit> {
 
     private static final String FILTER_BY_LOAN_CATEGORIES = "loanCategory";
@@ -26,11 +30,11 @@ public class ApprovalLimitSpec implements Specification<ApprovalLimit> {
     public Predicate toPredicate(Root<ApprovalLimit> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
         switch (property){
             case FILTER_BY_AUTHORITIES:
-                return criteriaBuilder.like(root.get(property), String.valueOf(value));
+                return criteriaBuilder.equal(root.join(FILTER_BY_AUTHORITIES).get("id"),Long.valueOf(value));
             case FILTER_BY_APPROVAL_TYPE:
-                return criteriaBuilder.like(root.get(property), value + "%");
+                return criteriaBuilder.equal(root.get(property), LoanApprovalType.valueOf(value));
             case FILTER_BY_LOAN_CATEGORIES:
-                return criteriaBuilder.like(root.get(property), String.valueOf(value));
+                return criteriaBuilder.equal(root.join(FILTER_BY_LOAN_CATEGORIES).get("id"),Long.valueOf(value));
             default:
                 return null;
         }
