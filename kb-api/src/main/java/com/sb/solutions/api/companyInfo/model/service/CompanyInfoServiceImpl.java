5a1e1f4686d5994cc6e75bbc25e3dd689f6b4840
sunilbau <whispersunil@gmail.com>
2021-02-25 16:40:51 +0545

    SRDB remove disable insurace / pan/vat check

--- kb-api/src/main/java/com/sb/solutions/api/companyInfo/model/service/CompanyInfoServiceImpl.java
+++ kb-api/src/main/java/com/sb/solutions/api/companyInfo/model/service/CompanyInfoServiceImpl.java
@@ -1,18 +1,11 @@
 package com.sb.solutions.api.companyInfo.model.service;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import lombok.extern.slf4j.Slf4j;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -28,14 +21,8 @@ import com.sb.solutions.api.customer.entity.CustomerInfo;
 import com.sb.solutions.api.customer.enums.CustomerIdType;
 import com.sb.solutions.api.customer.enums.CustomerType;
 import com.sb.solutions.api.customer.service.CustomerInfoService;
-import com.sb.solutions.api.loanflag.entity.CustomerLoanFlag;
 import com.sb.solutions.api.loanflag.service.CustomerLoanFlagService;
-import com.sb.solutions.api.preference.notificationMaster.entity.NotificationMaster;
-import com.sb.solutions.api.preference.notificationMaster.repository.spec.NotificationMasterSpec;
 import com.sb.solutions.api.preference.notificationMaster.service.NotificationMasterService;
-import com.sb.solutions.core.constant.AppConstant;
-import com.sb.solutions.core.enums.LoanFlag;
-import com.sb.solutions.core.enums.NotificationMasterType;
 import com.sb.solutions.core.exception.ServiceValidationException;
 
 
@@ -90,10 +77,12 @@ public class CompanyInfoServiceImpl implements CompanyInfoService {
                         .getName());
             }
 
-            if (!ObjectUtils.isEmpty(companyInfo.getPanNumber())){
-                CompanyInfo samePanNumber = companyInfoRepository.findCompanyInfoByPanNumber(companyInfo.getPanNumber());
+            if (!ObjectUtils.isEmpty(companyInfo.getPanNumber())) {
+                CompanyInfo samePanNumber = companyInfoRepository
+                    .findCompanyInfoByPanNumber(companyInfo.getPanNumber());
                 if (samePanNumber != null) {
-                    throw new ServiceValidationException("The pan number is already in use. You cannot create two clients with same pan number");
+                    throw new ServiceValidationException(
+                        "The pan number is already in use. You cannot create two clients with same pan number");
                 }
             }
         }
@@ -134,91 +123,92 @@ public class CompanyInfoServiceImpl implements CompanyInfoService {
 
     @Override
     public void execute(Long customerInfoId) {
-        Optional<CustomerInfo> customerInfo = customerInfoService.findOne(customerInfoId);
-        if (!customerInfo.isPresent()) {
-            log.error(
-                "Error checking company VAT/PAN expiry. Customer Info with id {} not found",
-                customerInfoId);
-            return;
-        }
-
-        Map<String, String> companyNotificationFilter = new HashMap<>();
-        companyNotificationFilter.put(
-            NotificationMasterSpec.FILTER_BY_NOTIFICATION_KEY,
-            NotificationMasterType.COMPANY_REGISTRATION_EXPIRY_BEFORE.toString()
-        );
-        Optional<NotificationMaster> notificationMaster = notificationMasterService
-            .findOneBySpec(companyNotificationFilter);
-        if (!notificationMaster.isPresent()) {
-            log.error("Company VAT/PAN Expiry Notification not configured");
-            return;
-        }
-
-        CustomerLoanFlag customerLoanFlag = customerInfo.get().getLoanFlags()
-            .stream()
-            .filter(loanFlag -> loanFlag.getFlag().equals(LoanFlag.COMPANY_VAT_PAN_EXPIRY))
-            .collect(CustomerLoanFlag.toSingleton());
-
-        CustomerLoanFlag customerEmptyLoanFlag = customerInfo.get().getLoanFlags()
-            .stream()
-            .filter(loanFlag -> loanFlag.getFlag().equals(LoanFlag.EMPTY_COMPANY_VAT_PAN_EXPIRY))
-            .collect(CustomerLoanFlag.toSingleton());
-
-
-        try {
-            if (!ObjectUtils
-                .isEmpty(this.findOne(customerInfo.get().getAssociateId()).getLegalStatus()
-                    .getRegistrationExpiryDate())) {
-            int daysToExpiryBefore = notificationMaster.get().getValue();
-            Calendar c = Calendar.getInstance();
-            c.setTime(new Date());
-            c.add(Calendar.DAY_OF_MONTH, daysToExpiryBefore);
-            SimpleDateFormat dateFormat = new SimpleDateFormat(AppConstant.MM_DD_YYYY);
-            Date today = dateFormat.parse(dateFormat.format(c.getTime()));
-            Date expiry = dateFormat.parse(dateFormat.format(
-                this.findOne(customerInfo.get().getAssociateId()).getLegalStatus()
-                    .getRegistrationExpiryDate()));
-            boolean flag = expiry.before(today);
-            if (flag && customerLoanFlag == null) {
-                customerLoanFlag = new CustomerLoanFlag();
-                customerLoanFlag.setCustomerInfo(customerInfo.get());
-                customerLoanFlag.setFlag(LoanFlag.COMPANY_VAT_PAN_EXPIRY);
-                customerLoanFlag.setDescription(
-                    String.format(LoanFlag.COMPANY_VAT_PAN_EXPIRY.getValue()[1],
-                        daysToExpiryBefore));
-                customerLoanFlag.setOrder(
-                    Integer.parseInt(LoanFlag.COMPANY_VAT_PAN_EXPIRY.getValue()[0]));
-                customerLoanFlagService.save(customerLoanFlag);
-            }
-            else if (!flag && customerLoanFlag != null) {
-                customerLoanFlagService
-                    .deleteCustomerLoanFlagById(customerLoanFlag.getId());
-            }
-            if (customerEmptyLoanFlag != null) {
-                customerLoanFlagService
-                    .deleteCustomerLoanFlagById(customerEmptyLoanFlag.getId());
-            }
-        } else {
-                if (customerLoanFlag != null) {
-                customerLoanFlagService
-                    .deleteCustomerLoanFlagById(customerLoanFlag.getId());
-                }
-                if (customerEmptyLoanFlag != null) {
-                    customerLoanFlagService
-                        .deleteCustomerLoanFlagById(customerEmptyLoanFlag.getId());
-                }
-                customerLoanFlag = new CustomerLoanFlag();
-                customerLoanFlag.setCustomerInfo(customerInfo.get());
-                customerLoanFlag.setFlag(LoanFlag.EMPTY_COMPANY_VAT_PAN_EXPIRY);
-                customerLoanFlag.setDescription(
-                    String.format(LoanFlag.EMPTY_COMPANY_VAT_PAN_EXPIRY.getValue()[1]));
-                customerLoanFlag.setOrder(
-                    Integer.parseInt(LoanFlag.EMPTY_COMPANY_VAT_PAN_EXPIRY.getValue()[0]));
-                customerLoanFlagService.save(customerLoanFlag);
-
-            }
-        } catch (ParseException e) {
-            log.error("Error parsing company registration expiry date");
-        }
+        //TODO disable pan vat
+//        Optional<CustomerInfo> customerInfo = customerInfoService.findOne(customerInfoId);
+//        if (!customerInfo.isPresent()) {
+//            log.error(
+//                "Error checking company VAT/PAN expiry. Customer Info with id {} not found",
+//                customerInfoId);
+//            return;
+//        }
+//
+//        Map<String, String> companyNotificationFilter = new HashMap<>();
+//        companyNotificationFilter.put(
+//            NotificationMasterSpec.FILTER_BY_NOTIFICATION_KEY,
+//            NotificationMasterType.COMPANY_REGISTRATION_EXPIRY_BEFORE.toString()
+//        );
+//        Optional<NotificationMaster> notificationMaster = notificationMasterService
+//            .findOneBySpec(companyNotificationFilter);
+//        if (!notificationMaster.isPresent()) {
+//            log.error("Company VAT/PAN Expiry Notification not configured");
+//            return;
+//        }
+//
+////        CustomerLoanFlag customerLoanFlag = customerInfo.get().getLoanFlags()
+////            .stream()
+////            .filter(loanFlag -> loanFlag.getFlag().equals(LoanFlag.COMPANY_VAT_PAN_EXPIRY))
+////            .collect(CustomerLoanFlag.toSingleton());
+////
+////        CustomerLoanFlag customerEmptyLoanFlag = customerInfo.get().getLoanFlags()
+////            .stream()
+////            .filter(loanFlag -> loanFlag.getFlag().equals(LoanFlag.EMPTY_COMPANY_VAT_PAN_EXPIRY))
+////            .collect(CustomerLoanFlag.toSingleton());
+//
+//
+//        try {
+//            if (!ObjectUtils
+//                .isEmpty(this.findOne(customerInfo.get().getAssociateId()).getLegalStatus()
+//                    .getRegistrationExpiryDate())) {
+//            int daysToExpiryBefore = notificationMaster.get().getValue();
+//            Calendar c = Calendar.getInstance();
+//            c.setTime(new Date());
+//            c.add(Calendar.DAY_OF_MONTH, daysToExpiryBefore);
+//            SimpleDateFormat dateFormat = new SimpleDateFormat(AppConstant.MM_DD_YYYY);
+//            Date today = dateFormat.parse(dateFormat.format(c.getTime()));
+//            Date expiry = dateFormat.parse(dateFormat.format(
+//                this.findOne(customerInfo.get().getAssociateId()).getLegalStatus()
+//                    .getRegistrationExpiryDate()));
+//            boolean flag = expiry.before(today);
+////            if (flag && customerLoanFlag == null) {
+////                customerLoanFlag = new CustomerLoanFlag();
+////                customerLoanFlag.setCustomerInfo(customerInfo.get());
+////                customerLoanFlag.setFlag(LoanFlag.COMPANY_VAT_PAN_EXPIRY);
+////                customerLoanFlag.setDescription(
+////                    String.format(LoanFlag.COMPANY_VAT_PAN_EXPIRY.getValue()[1],
+////                        daysToExpiryBefore));
+////                customerLoanFlag.setOrder(
+////                    Integer.parseInt(LoanFlag.COMPANY_VAT_PAN_EXPIRY.getValue()[0]));
+////                customerLoanFlagService.save(customerLoanFlag);
+////            }
+////            else if (!flag && customerLoanFlag != null) {
+////                customerLoanFlagService
+////                    .deleteCustomerLoanFlagById(customerLoanFlag.getId());
+////            }
+////            if (customerEmptyLoanFlag != null) {
+////                customerLoanFlagService
+////                    .deleteCustomerLoanFlagById(customerEmptyLoanFlag.getId());
+////            }
+//        } else {
+//                if (customerLoanFlag != null) {
+//                customerLoanFlagService
+//                    .deleteCustomerLoanFlagById(customerLoanFlag.getId());
+//                }
+//                if (customerEmptyLoanFlag != null) {
+//                    customerLoanFlagService
+//                        .deleteCustomerLoanFlagById(customerEmptyLoanFlag.getId());
+//                }
+//                customerLoanFlag = new CustomerLoanFlag();
+//                customerLoanFlag.setCustomerInfo(customerInfo.get());
+//                customerLoanFlag.setFlag(LoanFlag.EMPTY_COMPANY_VAT_PAN_EXPIRY);
+//                customerLoanFlag.setDescription(
+//                    String.format(LoanFlag.EMPTY_COMPANY_VAT_PAN_EXPIRY.getValue()[1]));
+//                customerLoanFlag.setOrder(
+//                    Integer.parseInt(LoanFlag.EMPTY_COMPANY_VAT_PAN_EXPIRY.getValue()[0]));
+//                customerLoanFlagService.save(customerLoanFlag);
+//
+//            }
+//        } catch (ParseException e) {
+//            log.error("Error parsing company registration expiry date");
+//        }
     }
 }
