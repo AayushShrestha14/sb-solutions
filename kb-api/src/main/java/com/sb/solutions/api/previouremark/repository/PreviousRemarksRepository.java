91a786890261279a67d98f5a75d43597c40ecb3d
aayushlalshrestha <mail2ayus@yahoo.com>
2021-07-02 11:52:03 +0545

    revert to master-v2

--- kb-api/src/main/java/com/sb/solutions/api/previouremark/repository/PreviousRemarksRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sb.solutions.api.previouremark.repository;
-
-import com.sb.solutions.api.previouremark.entity.PreviousRemarks;
-import com.sb.solutions.core.repository.BaseRepository;
-
-public interface PreviousRemarksRepository extends BaseRepository<PreviousRemarks, Long> {
-}
--- kb-api/src/main/java/com/sb/solutions/api/previouremark/service/PreviousRemarkService.java
+++
@@ -1,8 +0,0 @@
-package com.sb.solutions.api.previouremark.service;
-
-
-import com.sb.solutions.api.previouremark.entity.PreviousRemarks;
-import com.sb.solutions.core.service.Service;
-
-public interface PreviousRemarkService extends Service<PreviousRemarks, Long> {
-}
