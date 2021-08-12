06dad441a7e74dc260aa94b226bb228ca70ef9c3
sunil babu shrestha <whispersunil@gmail.com>
2021-08-12 04:59:42 +0000

    Merge branch 'delete-cad-document' into 'creditx_d'
    Delete cad document from database
    
    See merge request sunilbabu/sb-solution!635
    
    (cherry picked from commit c5b02a9378bd269c10d1057a90c71af38f363998)
    
    218ef542 Delete cad document from database

--- kb-api/src/main/java/com/sb/solutions/api/loan/repository/CadDocumentRepository.java
+++ kb-api/src/main/java/com/sb/solutions/api/loan/repository/CadDocumentRepository.java
@@ -3,7 +3,15 @@ package com.sb.solutions.api.loan.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sb.solutions.api.loan.entity.CadDocument;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import javax.transaction.Transactional;
 
 public interface CadDocumentRepository extends JpaRepository<CadDocument, Long> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "delete from customer_loan_cad_document where customer_loan_id=? AND cad_document_id=?", nativeQuery = true)
+    int deleteByLoanIdAndDocument(Long customerLoanId, Long docId);
 
 }
