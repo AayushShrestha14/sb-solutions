e2423f0ba4aad5df9c67f2727a4bd7b6abf71b1a
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-08-19 15:43:37 +0545

    pick site visit deletion feature from creditx_d_icfc_master to master_v2

--- sb-cad/src/main/java/com/sb/solutions/constant/SuccessMessage.java
+++ sb-cad/src/main/java/com/sb/solutions/constant/SuccessMessage.java
@@ -7,4 +7,5 @@ public class SuccessMessage {
 
     public static final String SUCCESS_ASSIGNED = "SUCCESSFULLY ASSIGNED LOAN DOCUMENT";
     public static final String SUCCESS_FORWARD = "SUCCESSFULLY FORWARDED LOAN DOCUMENT";
+    public static final String DELETE_SUCCESS = "DELETE SITE VISIT SUCCESSFUL";
 }
