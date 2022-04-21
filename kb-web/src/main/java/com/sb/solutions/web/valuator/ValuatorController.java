4ce776a1ca51f492d723cdbe15faf51560d4aa9b
KarmaGhalan2 <karma@sbsolutionsnepal.com>
2022-04-21 11:57:42 +0545

    remove import and make single

--- kb-web/src/main/java/com/sb/solutions/web/valuator/ValuatorController.java
+++ kb-web/src/main/java/com/sb/solutions/web/valuator/ValuatorController.java
@@ -5,7 +5,13 @@ import io.swagger.annotations.ApiImplicitParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.sb.solutions.api.user.service.UserService;
 import com.sb.solutions.api.valuator.entity.Valuator;
